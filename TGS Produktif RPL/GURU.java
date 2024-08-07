324909f9314b6d42fba35958275c34a0fcbb2dda
Mahardika-dika <mahardika_arfuri_33rpl@student.smktelkom-mlg.sch.id>
2024-08-07 14:40:15 +0700

    Add files via upload

---
+++ TGS Produktif RPL/GURU.java
@@ -0,0 +1,15 @@
+// Object Class
+
+public class GURU {
+    String Nama, Mapel, Alamat = "";
+    int id = 0;
+
+    // Constuctor Name same as Class Name
+    public GURU () {
+        Nama = "";
+        Mapel = "";
+        Alamat = "";
+        id = 0;
+    }
+    
+}
---
+++ TGS Produktif RPL/TestGuru.java
@@ -0,0 +1,55 @@
+public class TestGuru {
+    public static void main(String[] args) {
+
+        GURU PashataniaFitriIndahLestari = new GURU();
+        PashataniaFitriIndahLestari.id = 1 ;
+        PashataniaFitriIndahLestari.Nama = "Pashatania Fitri Indah Lestari ";
+        PashataniaFitriIndahLestari.Mapel = "Produktif RPL ";
+        PashataniaFitriIndahLestari.Alamat = "Malang";
+        GURU AuliaMasadah = new GURU();
+        AuliaMasadah.id = 2 ;
+        AuliaMasadah.Nama = "Aulia Mas'adah ";
+        AuliaMasadah.Mapel = "Produktif RPL";
+        AuliaMasadah.Alamat = "Malang";
+        GURU DiaurRahman = new GURU();
+        DiaurRahman.id = 3 ;
+        DiaurRahman.Nama = "Diaur Rahman";
+        DiaurRahman.Mapel = "IPAS, Pendidikan Jasmani";
+        DiaurRahman.Alamat = "Malang";
+        GURU FirmansyahAyatullah = new GURU();
+        FirmansyahAyatullah.id = 4 ;
+        FirmansyahAyatullah.Nama = "Firmansyah Ayatullah";
+        FirmansyahAyatullah.Mapel = "Informatika (ui/ux design)";
+        FirmansyahAyatullah.Alamat = "Malang";
+        GURU AgusHariPurwanto = new GURU();
+        AgusHariPurwanto.id = 5 ;
+        AgusHariPurwanto.Nama = "Agus Hari Purwanto";
+        AgusHariPurwanto.Mapel = "Bahasa Indonesia";
+        AgusHariPurwanto.Alamat = "Malang";
+
+        System.out.println("DAFTAR NAMA GURU: ");
+        System.out.println(PashataniaFitriIndahLestari.id);
+        System.out.println(PashataniaFitriIndahLestari.Nama);
+        System.out.println(PashataniaFitriIndahLestari.Mapel);
+        System.out.println(PashataniaFitriIndahLestari.Alamat);
+        System.out.println(AuliaMasadah.id);
+        System.out.println(AuliaMasadah.Nama);
+        System.out.println(AuliaMasadah.Mapel);
+        System.out.println(AuliaMasadah.Alamat);
+        System.out.println(DiaurRahman.id);
+        System.out.println(DiaurRahman.Nama);
+        System.out.println(DiaurRahman.Mapel);
+        System.out.println(DiaurRahman.Alamat);
+        System.out.println(FirmansyahAyatullah.id);
+        System.out.println(FirmansyahAyatullah.Nama);
+        System.out.println(FirmansyahAyatullah.Mapel);
+        System.out.println(FirmansyahAyatullah.Alamat);
+        System.out.println(AgusHariPurwanto.id);
+        System.out.println(AgusHariPurwanto.Nama);
+        System.out.println(AgusHariPurwanto.Mapel);
+        System.out.println(AgusHariPurwanto.Alamat);
+
+        
+    }
+
+}
