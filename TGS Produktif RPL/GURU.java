774772db151f703ef0d9ce9a2d1464f1a4544d23
Mahardika-dika <mahardika_arfuri_33rpl@student.smktelkom-mlg.sch.id>
2024-08-21 19:47:55 +0700

    Add files via upload

--- TGS Produktif RPL/GURU.java
+++ TGS Produktif RPL/GURU.java
@@ -1,15 +1,33 @@
 // Object Class
 
 public class GURU {
-    String Nama, Mapel, Alamat = "";
-    int id = 0;
+    String Nama, Mapel, Alamat;
+    int id;
 
     // Constuctor Name same as Class Name
-    public GURU () {
+    public GURU() {
         Nama = "";
         Mapel = "";
         Alamat = "";
         id = 0;
     }
-    
+
+    // constructor parameter
+    public GURU(int id, String Nama, String Alamat, String Mapel) {
+        this.id = id;
+        this.Mapel = Mapel;
+        this.Alamat = Alamat;
+        this.Nama = Nama;
+    }
+
+    // Method
+    public void print() {
+        System.out.println("|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|");
+        System.out.println("ID: " + id);
+        System.out.println("NAMA: " + Nama);
+        System.out.println("MAPEL: " + Mapel);
+        System.out.println("ALAMAT: " + Alamat);
+        System.out.println("|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|");
+    }
+
 }
