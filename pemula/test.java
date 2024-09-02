11391a418fef861a27300f7291781ad729696fac
Mahardika-dika <mahardika_arfuri_33rpl@student.smktelkom-mlg.sch.id>
2024-09-02 20:50:46 +0700

    Delete pemula directory

--- pemula/test.java
+++
@@ -1,9 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        System.out.println("Nama: Mahardika Arfuri");
-        System.out.println("Alamat: Jl. Raya Ki Ageng Gribig, Madyopuro, Kec. Kedungkandang, Kota Malang, Jawa Timur Perumahan Riverfront B51");
-        System.out.println("SMP: MTSn 2 Kota Malang");
-        System.out.println("No HP: +62 857-3264-9015");
-    }
-    
-}
